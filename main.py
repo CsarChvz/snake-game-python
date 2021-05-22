30f3367ba49c6b75283b170a00419ccaee2af281
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-05-22 13:23:00 -0500

    Cuadro que se puede mover owo, sin POO

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 import pygame
 from pygame.locals import *
 
+def draw_block():
+    surface.fill((110, 110, 5))
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
 
 if __name__ == '__main__':
 
-   #Event Loop es importante para todos los programas de UI
-    def event_loop():
-        """ EVENT LOOP """
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        running = False
-                elif event.type == QUIT:
-                    running = False
-
-    # Funcion para mostrar el bloque en la pantalla surface
-
-    def draw_block(block_y, block_x):
-        surface.fill((255, 255, 254))
-        block_x = 100
-        block_y = 100
-        block = pygame.image.load("resources/block.jpg").convert()
-        surface.blit(block, (block_x, block_y))
-
-    # Se termina el event loop para cerrar la ventana sin tener un timeer
     pygame.init()
-    surface = pygame.display.set_mode((600, 600))
-    draw_block()
-    pygame.display.flip()
-    event_loop()
-    # Para mover nuestro cuadrado necesitamos moverlo en nuestra funcion event lopp 
+    
+    surface = pygame.display.set_mode((500, 500))
+    surface.fill((110, 110, 5))
 
+    block = pygame.image.load("resources/block.jpg").convert()
+
+    block_x, block_y = 100, 100
+
+    surface.blit(block, (block_x, block_y))
+
+    pygame.display.flip()
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+                if event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+            elif event.type == QUIT:
+                running = False
