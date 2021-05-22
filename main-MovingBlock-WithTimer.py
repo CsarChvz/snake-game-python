ba34f22212ffe3067cd616bab65f734a800856ce
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-05-22 16:28:39 -0500

    Cambio en la detección de dirección
    Lo que se cambio fue que a la hora que el cuadrado siguiera avanzando y
    no estuvieramos picando la tecla, entonces se hizo una funcion en la
    clase snake para que este caminara segun la direccion que picaramos:
    
    Podemos ver que segun la direccion que guardaba, entonces si los cambios
    locales o las teclas detectaban que habia una pulsacion, entonces
    activaban un metodo de la serpiente, que era mover hacia el lado que
    picamos, entonces la funcion se ejecutaba y este lo que hacia era poner
    una variable y ponerle el valor segun la direccion que iba, entonces
    esta variable se renombraba y guradaba otro valor, entonces a la hora de
    avanzar, lo que se hacia  era ver que condiucion estaba cumpleando y
    sumar o restar

--- main-MovingBlock-WithTimer.py
+++ main-MovingBlock-WithTimer.py
@@ -1,29 +1,37 @@
 import pygame
 from pygame.locals import *
-
+import time
 class Snake:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen 
         self.block = pygame.image.load("resources/block.jpg").convert()
         self.x = 100
         self.y = 100
-    
+        self.direction = 'down'
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
         self.parent_screen.blit(self.block, (self.x, self.y))
         pygame.display.flip()
  
     def move_left(self):
-        self.x -= 10
-        self.draw()
+        self.direction = 'left'
     def move_right(self):
-        self.x += 10
-        self.draw()
+        self.direction = 'right'
     def move_up(self):
-        self.y -= 10
-        self.draw()
+        self.direction = 'up'
     def move_down(self):
-        self.y += 10
+        self.direction = 'down'
+
+    def walk(self):
+        if self.direction == 'left':
+            self.x -= 10
+        if self.direction == 'right':
+            self.x += 10
+        if self.direction == 'up':
+            self.y -= 10
+        if self.direction == 'down':
+            self.y += 10
+
         self.draw()
 
 class Game:
@@ -51,12 +59,14 @@ class Game:
                     if event.key == K_RIGHT:
                         self.snake.move_right()
                     if event.key == K_UP:
-                        self.snake_move_up()
+                        self.snake.move_up()
                     if event.key == K_DOWN:
                         self.snake.move_down()
                 elif event.type == QUIT:
                     running = False
 
+            self.snake.walk()
+            time.sleep(0.2)
 
 if __name__ == '__main__':
     game = Game()
